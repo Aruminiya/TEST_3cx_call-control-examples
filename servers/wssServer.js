c8b9495c55f3f965f3569bb660dc6110576fd8fa
Aruminiya <leo80103@gmail.com>
2024-12-19 17:38:31 +0800

    feat: 有一點眉目

--- servers/wssServer.js
+++ servers/wssServer.js
@@ -5,8 +5,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
+const token_3cx = require('../utils/getToken');
 
 dotenv.config();
+
+
+// connectTo3CX()
 
 app.use(cors());
 const WS_PORT = process.env.WS_PORT || 3082;
@@ -24,7 +28,8 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log('Received:', message);
     // 在這裡可以處理來自客戶端的訊息
-    // ws3cx.send(message);
+    // 將訊息發送回客戶端
+    ws.send(`Echo 訊息發送回客戶端: ${message}`);
   });
 
   // 當連接關閉時
@@ -43,10 +48,11 @@ wss.on('connection', (ws) => {
 
 
 
-// // 創建WebSocket客戶端連接到3CX
+// 創建WebSocket客戶端連接到3CX
+// const token = await token_3cx();
 // const ws3cx = new WebSocket('wss://bonuc.3cx.com.tw/callcontrol/ws', {
 //   headers: {
-//     'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`, // 替換為您的Token
+//     'Authorization': `Bearer eyJhbGciOiJFUzI1NiIsImtpZCI6ImpWbHdJWXlNRmoxMk5HNXFaZU1qUWciLCJ0eXAiOiJKV1QifQ.eyJ1bmlxdWVfbmFtZSI6InZpY3RvciIsImp0aSI6IjMyNWQ1YTNlLTY0YTktNDYwNi1hYmNhLTU5YmJhZTlkYTZmNiIsInJvbGUiOlsiQXBwIiwiUmVwb3J0cyIsIlBob25lU3lzdGVtQWRtaW4iLCJHcm91cHMuQ3JlYXRlIiwiVXNlcnMiLCJUcnVua3MiLCJHbG9iYWxBZG1pbiIsIkFkbWluIiwiQXBwcy5SZWFkV3JpdGUiLCJNYWNoaW5lQWRtaW4iLCJTaW5nbGVDb21wYW55IiwiUGFpZCIsIkVudGVycHJpc2UiLCJDYWxsRmxvd0FwcCJdLCJNYXhSb2xlIjoic3lzdGVtX293bmVycyIsIm5iZiI6MTczNDU5OTI3MywiZXhwIjoxNzM0NjAyODczLCJpYXQiOjE3MzQ1OTkyNzMsImlzcyI6ImJvbnVjLjNjeC5jb20udHciLCJhdWQiOiIvYXBpIn0.WwKnlQ8puS-jO5ikm6_wu9TqUlQ7tSTId2XDJApeb7NWaf4iqAPGUoI-SGmHiIxgUuu1dOvzSBiQbaij30ZdbA`, // 替換為您的Token
 //   }
 // });
 
@@ -71,6 +77,7 @@ wss.on('connection', (ws) => {
 // ws3cx.on('error', (error) => {
 //   console.error('3CX WebSocket error:', error);
 // });
+
 
 // 啟動伺服器
 server.listen(WS_PORT, () => {
